ded1ee784ea0df756e53ec475fced82b35f5e3d3
biluo1989 <biluo.han@gmail.com>
2013-08-18 19:05:43 +0800

    Twelve cards can be flipped  for different suit and rank.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,14 +7,31 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtom;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtoms;
+@property (nonatomic, strong) Deck *deck;
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void)setCardButtoms:(NSArray *)cardButtoms
+{
+    _cardButtoms = cardButtoms;
+    for (UIButton *cardButtom in cardButtoms){
+        Card *card = [self.deck drawRondomCard];
+        [cardButtom setTitle:card.contents forState:UIControlStateSelected];
+    }
+}
 
 - (void)setFlipCount:(int)flipCount
 {
